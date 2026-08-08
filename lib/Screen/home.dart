b85acfe8a927005db51f738dc07b11a2082ce767
Santikan07 <wsgegdg@gmail.com>
2026-08-08 14:08:18 +0700

    Stap 6: Image Widget

--- lib/Screen/home.dart
+++ lib/Screen/home.dart
@@ -9,14 +9,9 @@ class Home extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Image.asset(
-          'assets/images/1.webp',
-          width: 200,
-          height: 200,
-        ),
-
-        const SizedBox(height: 20),
-
+        Image.asset('assets/images/1.webp',width: 200,height: 200,),
+        SizedBox(height: 20),
+      
         Image.network(
           'https://th.bing.com/th/id/OIP.4kbGF9fdAe8odnPtqvZxdQHaHj?w=176&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3',
           width: 200,
