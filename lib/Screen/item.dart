be2063a813ab306674b35ccd492b2749b321995a
Santikan07 <wsgegdg@gmail.com>
2026-08-15 11:10:49 +0700

    Step 10: Make a model for store data

--- lib/Screen/item.dart
+++ lib/Screen/item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:learn_flutter_68_1/model/peson.dart';
 
 class Item extends StatefulWidget {
   const Item({super.key});
@@ -8,35 +9,46 @@ class Item extends StatefulWidget {
 }
 
 class _ItemState extends State<Item> {
-  List data = [
-    "สมชาย",
-    "สมศรี",
-    "สมปอง",
-    "สมหมาย",
-    "สมจิตร",
-    "สมพร",
-    "สมบัติ",
-    "สมฤดี",
-    "สมศักดิ์",
-    "สมใจ",
-  ];
-
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: data.length,
+      itemCount: people.length,
       itemBuilder: (context, index) {
         return Container(
           decoration: BoxDecoration(
             border: Border.all(color: Colors.black),
             borderRadius: BorderRadius.circular(10),
           ),
-          margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-          padding: EdgeInsets.all(30),
-          child: Text(
-            data[index],
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          )
+          margin: const EdgeInsets.symmetric(
+            vertical: 5,
+            horizontal: 10,
+          ),
+          padding: const EdgeInsets.all(30),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    people[index].name,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    "อายุ: ${people[index].age}",
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                  Text(
+                    "อาชีพ: ${people[index].job}",
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                ],
+              ),
+            ],
+          ),
         );
       },
     );
---
+++ lib/model/peson.dart
@@ -0,0 +1,20 @@
+class Person {
+  String name;
+  int age;
+  String job;
+
+  Person({required this.name, required this.age, required this.job});
+}
+
+List<Person> people = [
+  Person(name: "สมชาย", age: 30, job: "นักเรียน"),
+  Person(name: "สมศรี", age: 25, job: "นักศึกษา"),
+  Person(name: "สมปอง", age: 40, job: "พนักงานบริษัท"),
+  Person(name: "สมหมาย", age: 35, job: "ครู"),
+  Person(name: "สมจิตร", age: 28, job: "นักธุรกิจ"),
+  Person(name: "สมพร", age: 32, job: "แพทย์"),
+  Person(name: "สมบัติ", age: 45, job: "วิศวกร"),
+  Person(name: "สมฤดี", age: 38, job: "นักวิจัย"),
+  Person(name: "สมศักดิ์", age: 50, job: "ผู้จัดการ"),
+  Person(name: "สมใจ", age: 27, job: "นักออกแบบ"),
+];
